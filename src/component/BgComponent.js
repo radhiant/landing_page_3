202314d5703718abd829a48e1fe71994effa6ebf
radhiant <radhiantsobarna@gmail.com>
2021-05-20 13:21:57 +0700

    update code

--- src/component/BgComponent.js
+++ src/component/BgComponent.js
@@ -3,11 +3,11 @@ import React, { Component } from 'react'
 export default class BgComponent extends Component {
     render() {
         return (
-            <section>
+            <div>
                 <img src="assets/header-bg.svg" className="position-absolute top-0 end-0 d-none d-lg-block" alt="bg" />
-                <img src="assets/header-vector.svg" className="position-absolute img-header top-0 end-0 d-none d-lg-block" alt="bg" />
+                <img src="assets/header-vector.svg" className="position-absolute img-header top-0 end-0 d-lg-block" alt="bg" />
                 <img src="assets/header-vector.svg" width="100%" className="display-header" alt="bg" />
-            </section>
+            </div>
         )
     }
 }
